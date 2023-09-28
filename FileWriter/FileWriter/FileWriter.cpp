eb6c7036ba67c8260133881ae05844f4135e0f4d
Rrazr <110663400+Rrazr@users.noreply.github.com>
2023-09-28 15:23:31 -0700

    Update FileWriter.cpp

--- FileWriter/FileWriter/FileWriter.cpp
+++ FileWriter/FileWriter/FileWriter.cpp
@@ -7,7 +7,9 @@
 using namespace std;
 
 int main(){
-    ofstream outpfile;
+    // Code for creating and deleting files
+    
+    /*ofstream outpfile;
     string comm, pa = "";
 
     while (1) {
@@ -37,15 +39,27 @@ int main(){
             }
         }
         cout << '\n';
+    }*/
+    
+    
+    // Code for reading files
+    
+    ofstream outputfile;
+    ifstream inputfile;
+    string path;
+    int nums;
+    
+    cout << "Enter filename to read: ";
+    cin >> path;
+    inputfile.open(path);
+    
+    if (inputfile){
+        cout << "Success!\n";
+        while (inputfile >> nums){
+            cout << nums << '\n';
+        }
+    }else{
+        cout << "Failure\n";
     }
+    inputfile.close();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
